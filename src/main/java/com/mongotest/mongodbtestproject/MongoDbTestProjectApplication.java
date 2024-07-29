170876006dc5150f64a0e0e42bdf7d9eba91871c
Maryam sadat <maryam.s01382@gmail.com>
2024-07-29 15:48:06 +0330

    init

---
+++ src/main/java/com/mongotest/mongodbtestproject/MongoDbTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.mongotest.mongodbtestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbTestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoDbTestProjectApplication.class, args);
+    }
+
+}
